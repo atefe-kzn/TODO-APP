b7ffd27f3bb5101f9354a81e8be9852e95c43be1
atefe.kzn <atefeh.khoozan@gmail.com>
2019-02-07 16:07:28 +0330

    create list - add item function - remove item

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./TodoList.css";
 import TodoApp from "./TodoApp";
   
 var destination = document.querySelector("#container");
